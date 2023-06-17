ad9f65822bd3edbeea1e058c54b543396d1914f2
NazmulHasan000 <nazmulhasanshadin000@gmail.com>
2023-06-17 15:06:37 +0600

    update

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 import { faCartShopping  } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 const Product = (props) => {
