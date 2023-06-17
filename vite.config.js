ee75c0975425cf8be447d94973bdb56aeada9837
NazmulHasan000 <nazmulhasanshadin000@gmail.com>
2023-06-17 14:31:24 +0600

    j

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import react from '@vitejs/plugin-react'
+// vite.config.js
+
+import react from '@vitejs/plugin-react';
 
 export default {
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@fortawesome/free-solid-svg-icons']
+      external: [
+        '@fortawesome/free-solid-svg-icons',
+        '@fortawesome/react-fontawesome'
+      ]
     }
   }
-}
+};
